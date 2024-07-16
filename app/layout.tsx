0a73e0de8e239cb28ada0273774e821efcb1f24d
Rasel Hossen <raselhossen052@gmail.com>
2024-07-16 18:23:17 +0600

    implement-theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -23,11 +24,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "bg-dark-300 max-h-screen font-sans antialiased",
+          "max-h-screen bg-dark-300 font-sans antialiased",
           fontSans.variable,
         )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
