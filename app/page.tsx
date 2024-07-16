3deec4f3b5ebdd583a3d9de91d2324fd49d6b238
Rasel Hossen <raselhossen052@gmail.com>
2024-07-16 18:24:08 +0600

    care-pulse-initial-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-white">
-      <h1>Home</h1>
+    <main className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+        <PatientForm/>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PatientForm() {
+  return (
+    <div>PatientForm</div>
+  )
+}
